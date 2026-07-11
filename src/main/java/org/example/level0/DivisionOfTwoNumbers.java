77439ab3f3f554b5cbdb0e51d597cf59d292158e
data8320-22 <rlaehddnwkd1107@gmail.com>
2026-07-11 17:03:26 +0900

    두 수의 곱 구하기

---
+++ src/main/java/org/example/level0/DivisionOfTwoNumbers.java
@@ -0,0 +1,19 @@
+package org.example.level0;
+
+public class DivisionOfTwoNumbers {
+    public static void main(String[] args) {
+        /*
+        정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에
+        1,000을 곱한 후 정수 부분을 return 하도록 solution 함수를 완성해주세요.
+         */
+        int num1 = 3;
+        int num2 = 2;
+        System.out.println(solution(num1, num2));
+    }
+    static int solution(int num1, int num2) {
+        int answer = num1 *1000/ num2;
+        return answer;
+    }
+}
+
+
