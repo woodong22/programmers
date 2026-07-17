0acb3960b3d60fe4c67f3ccd740cd51ba74f5697
data8320-22 <rlaehddnwkd1107@gmail.com>
2026-07-17 16:35:57 +0900

    피자 나눠 먹기(1)

---
+++ src/main/java/org/example/level0/PizzaSharing1.java
@@ -0,0 +1,22 @@
+package org.example.level0;
+
+public class PizzaSharing1 {
+    public static void main(String[] args) {
+        /*
+        머쓱이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다. 피자를 나눠먹을 사람의 수 n이 주어질 때, 모든 사람이 피자를 한 조각 이상 먹기 위해 필요한
+        피자의 수를 return 하는 solution 함수를 완성해보세요.
+         */
+        int n = 7;
+        System.out.println(Solution(n));
+    }
+
+    static int Solution(int n) {
+        int answer = n / 7;
+
+        if (n % 7 != 0) {
+            answer++;
+        }
+
+        return answer;
+    }
+}
